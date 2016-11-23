c20fd45f8e38175dd3b208ba0a8c35935aad6d01
magdaraf1 <magdaraf1@users.noreply.github.com>
2016-11-23 20:49:58 +0200

    Update Betting.java

--- Betting.java
+++ Betting.java
@@ -6,7 +6,8 @@
 package Core;
 
 /**
- *
+ *i sinartsi betting antiproswpeuei ena sugkekrimeno eidos gurou pou klironomei ti mode opoy o paiktis prepei prwta
+ * na pontarei kapoious pontous kai meta na apantisei stin erwtisi an einai swsti i apantisi tous kerdizei an oxi tous xanei
  * @author Μάγδα Πατσιάγγουρα Κώστας Πατσάρας
  */
 public class Betting extends Mode {
